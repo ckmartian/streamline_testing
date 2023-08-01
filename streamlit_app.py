25f70a1d2fa3d4a1b821459affd6ce49960849ed
Christopher Kaminski <47246914+ckmartian@users.noreply.github.com>
2023-08-01 16:17:04 -0400

    big menu changes
    big menu changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 
-streamlit.title('this is the big title')
-streamlit.text('first line')
-streamlit.text('second line')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑Avocado Toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
